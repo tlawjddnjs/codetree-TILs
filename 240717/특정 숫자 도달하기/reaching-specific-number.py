a26d23865785e7409cc35b1b42f96c1e5ff483ea
tlawjddnjs <157342006+tlawjddnjs@users.noreply.github.com>
2024-07-17 19:21:30 +0900

    Title: 특정 숫자 도달하기, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240717/특정 숫자 도달하기/reaching-specific-number.py
+++ 240717/특정 숫자 도달하기/reaching-specific-number.py
@@ -1,11 +1,13 @@
 numlist = list(map(int, input().split()))
 total=0
+cnt=0
 for i in range(0, 10):
     if numlist[i]>=250:
         break
     else:
         total+=numlist[i]
+        cnt=i+1
 
 print(total, end=" ")
-avr=total/i
+avr=total/cnt
 print("%0.1f"%avr)
